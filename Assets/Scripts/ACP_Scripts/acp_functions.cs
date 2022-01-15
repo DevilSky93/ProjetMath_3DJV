593c62ed419405f5f3d12af8fa629a9f30c4e3a4
YumiD <gacon.dorian@gmail.com>
2022-01-16 00:25:26 +0100

    Update bones

--- Assets/Scripts/ACP_Scripts/acp_functions.cs
+++ Assets/Scripts/ACP_Scripts/acp_functions.cs
@@ -43,7 +43,29 @@ public class acp_functions : MonoBehaviour
         TestPoints.Add(ground1p4);
     }
 
-    Vector3 calculateBarycenter(List<Vector3> points)
+    void mainTest()
+    {
+        testList();
+
+        TestPoints = centrateDatas(TestPoints);
+
+        Matrix3x3 matCov = matrixCov(TestPoints);
+
+        //matCov.display();
+
+        Vector3 eigenvector = Eigenvector(matCov);
+
+        //print("eigenvector : " + eigenvector.x + " " + eigenvector.y + " " + eigenvector.z);
+
+        List<Vector3> TestProjections = projectedDatas(TestPoints, eigenvector);
+
+        (Vector3, Vector3) segmentMinMax = projectedDatasExtremes(TestPoints, TestProjections, eigenvector);
+
+        //print("min : " + segmentMinMax.Item1.x + " " + segmentMinMax.Item1.y + " " + segmentMinMax.Item1.z);
+        //print("max : " + segmentMinMax.Item2.x + " " + segmentMinMax.Item2.y + " " + segmentMinMax.Item2.z);
+    }
+
+    public Vector3 calculateBarycenter(List<Vector3> points)
     {
         float bary_x=0f, bary_y = 0f, bary_z = 0f;
         float somme_x = 0f, somme_y = 0f, somme_z = 0f;
@@ -63,7 +85,7 @@ public class acp_functions : MonoBehaviour
         return new Vector3(bary_x, bary_y, bary_z);
     }
 
-    List<Vector3> centrateDatas(List<Vector3> points)
+    public List<Vector3> centrateDatas(List<Vector3> points)
     {
         Vector3 barycenter = calculateBarycenter(TestPoints);
 
@@ -79,7 +101,7 @@ public class acp_functions : MonoBehaviour
         return v_centrateDatas;
     }
 
-    Matrix3x3 matrixCov(List<Vector3> points)
+    public Matrix3x3 matrixCov(List<Vector3> points)
     {
         Matrix3x3 matCov = new Matrix3x3();
         double covariance;
@@ -103,10 +125,20 @@ public class acp_functions : MonoBehaviour
 
     float maxAbsValue(Vector3 v3)
     {
-        return Mathf.Max(Mathf.Max(Mathf.Abs(v3.x), Mathf.Abs(v3.y)), Mathf.Abs(v3.z));
+        //return Mathf.Max(Mathf.Max(Mathf.Abs(v3.x), Mathf.Abs(v3.y)), Mathf.Abs(v3.z));
+        //Retourner LA PLUS GRANDE COMPOSANTE
+        if(Mathf.Abs(v3.x)< Mathf.Abs(v3.y))
+        {
+            if (Mathf.Abs(v3.y) < Mathf.Abs(v3.z))
+                return v3.z;
+            return v3.y;
+        }
+        if (Mathf.Abs(v3.x) < Mathf.Abs(v3.z))
+            return v3.z;
+        return v3.x;
     }
 
-    Vector3 Eigenvector(Matrix3x3 matCov)
+    public Vector3 Eigenvector(Matrix3x3 matCov)
     {
         Vector3 v0 = new Vector3(0f, 0f, 1f);
         int ITERATIONS = 100;
@@ -126,7 +158,7 @@ public class acp_functions : MonoBehaviour
         return vk.normalized;
     }
 
-    List<Vector3> projectedDatas(List<Vector3> points, Vector3 eigenvector)
+    public List<Vector3> projectedDatas(List<Vector3> points, Vector3 eigenvector)
     {
         List<Vector3> projectedDatas = new List<Vector3>();
         float x, y, z;
@@ -147,13 +179,7 @@ public class acp_functions : MonoBehaviour
         return projectedDatas;
     }
 
-    //Inutile?
-    float lengthPointFromOrigin(Vector3 point)
-    {
-        return Mathf.Sqrt(Mathf.Pow(point.x, 2) + Mathf.Pow(point.y, 2) + Mathf.Pow(point.z, 2));
-    }
-
-    (Vector3, Vector3) projectedDatasExtremes(List<Vector3> points_G, List<Vector3> points_projected, Vector3 eigenvector)
+    public (Vector3, Vector3) projectedDatasExtremes(List<Vector3> points_G, List<Vector3> points_projected, Vector3 eigenvector)
     {
         Vector3 min = new Vector3();
         float minAlpha = 0;
@@ -213,7 +239,7 @@ public class acp_functions : MonoBehaviour
             }
         }
 
-        if (minIndex != -1)
+        /*if (minIndex != -1)
         {
             min.x = points_projected[minIndex].x + points_G[minIndex].x;
             min.y = points_projected[minIndex].y + points_G[minIndex].y;
@@ -225,12 +251,15 @@ public class acp_functions : MonoBehaviour
             max.x = points_projected[maxIndex].x + points_G[maxIndex].x;
             max.y = points_projected[maxIndex].y + points_G[maxIndex].y;
             max.z = points_projected[maxIndex].z + points_G[maxIndex].z;
-        }
+        }*/
+        Vector3 barycenter = calculateBarycenter(points_G);
+        if (minIndex != -1)
+            min = points_projected[minIndex] + barycenter;
+        if (maxIndex != -1)
+            max = points_projected[maxIndex] + barycenter;
 
 
         (Vector3, Vector3) pMinMax = (min, max);
-        /*print("min : " + pMinMax.Item1.x + " " + pMinMax.Item1.y + " " + pMinMax.Item1.z);
-        print("max : " + pMinMax.Item2.x + " " + pMinMax.Item2.y + " " + pMinMax.Item2.z);*/
 
         return pMinMax;
     }
@@ -238,22 +267,7 @@ public class acp_functions : MonoBehaviour
 
     void Start()
     {
-        testList();
-
-        TestPoints = centrateDatas(TestPoints);
-
-        Matrix3x3 matCov = matrixCov(TestPoints);
-
-        //matCov.display();
-
-        Vector3 eigenvector = Eigenvector(matCov);
-
-        //print("eigenvector : " + eigenvector.x + " " + eigenvector.y + " " + eigenvector.z);
-
-        List<Vector3> TestProjections = projectedDatas(TestPoints, eigenvector);
-
-        (Vector3, Vector3) segmentMinMax = projectedDatasExtremes(TestPoints, TestProjections, eigenvector);
-
+        mainTest();
     }
 
     // Update is called once per frame
--- Assets/Scripts/ACP_Scripts/acp_mesh.cs
+++ Assets/Scripts/ACP_Scripts/acp_mesh.cs
@@ -8,18 +8,64 @@ using UnityEngine;
 public class acp_mesh : MonoBehaviour
 {
     Mesh mesh;
+    List<Vector3> mesh_vertices = new List<Vector3>();
+    public acp_functions acp_Functions;
+    public GameObject GO_point_min;
+    public GameObject GO_point_max;
+    public GameObject GO_point_black;
+    private (Vector3, Vector3) _segmentMinMax;
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(this._segmentMinMax.Item1, this._segmentMinMax.Item2);
+    }
 
     void Start()
     {
-        mesh = new Mesh();
+        //_____INIT______
+
         mesh = gameObject.GetComponent<MeshFilter>().mesh;
-        int cpt = 0;
-        foreach(Vector3 v in mesh.vertices)
+
+        //Position
+        foreach (Vector3 v in mesh.vertices)
+            mesh_vertices.Add(v + gameObject.transform.position);
+
+        //Rotation
+        Renderer rend = gameObject.GetComponent<Renderer>();
+        Vector3 center = rend.bounds.center;
+        Quaternion newRotation = new Quaternion();
+        newRotation.eulerAngles = new Vector3(gameObject.transform.localRotation.eulerAngles.x, gameObject.transform.localRotation.eulerAngles.y, gameObject.transform.localRotation.eulerAngles.z);
+        for (int i=0; i<mesh_vertices.Count; i++)
         {
-            //print(v);
-            cpt++;
+            mesh_vertices[i] = newRotation * (mesh_vertices[i] - center) + center;
+            if(GO_point_black != null)
+                Instantiate(GO_point_black, mesh_vertices[i], Quaternion.identity);
         }
-        print(cpt);
+
+
+        //______ACP______
+
+        mesh_vertices = acp_Functions.centrateDatas(mesh_vertices);
+
+        Matrix3x3 matCov = acp_Functions.matrixCov(mesh_vertices);
+
+        //matCov.display();
+
+        Vector3 eigenvector = acp_Functions.Eigenvector(matCov);
+
+        //print("eigenvector : " + eigenvector.x + " " + eigenvector.y + " " + eigenvector.z);
+
+        List<Vector3> mesh_vertices_projections = acp_Functions.projectedDatas(mesh_vertices, eigenvector);
+
+        _segmentMinMax = acp_Functions.projectedDatasExtremes(mesh_vertices, mesh_vertices_projections, eigenvector);
+
+        print("min : " + _segmentMinMax.Item1.x + " " + _segmentMinMax.Item1.y + " " + _segmentMinMax.Item1.z);
+        print("max : " + _segmentMinMax.Item2.x + " " + _segmentMinMax.Item2.y + " " + _segmentMinMax.Item2.z);
+
+        Instantiate(GO_point_min, _segmentMinMax.Item1, Quaternion.identity);
+        Instantiate(GO_point_max, _segmentMinMax.Item2, Quaternion.identity);
     }
 
     void Update()
