1cd0b6fda52496aa798e4f3c8d68f5d077c1191d
YumiD <gacon.dorian@gmail.com>
2022-01-21 15:58:57 +0100

    Update Joint

--- Assets/Scripts/ACP_Scripts/acp_create_bones.cs
+++ Assets/Scripts/ACP_Scripts/acp_create_bones.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class acp_create_bones : MonoBehaviour
 {
     public acp_functions acp_Functions;
+    public float _epsilon;
     List<Transform> _trans = new List<Transform>();
     List<MeshFilter> _meshFilter = new List<MeshFilter>();
     List<(Vector3, Vector3)> _segmentMinMax = new List<(Vector3, Vector3)>();
@@ -63,9 +64,74 @@ public class acp_create_bones : MonoBehaviour
 
         //print("min : " + _segmentMinMax[i].Item1.x + " " + _segmentMinMax[i].Item1.y + " " + _segmentMinMax[i].Item1.z);
         //print("max : " + _segmentMinMax[i].Item2.x + " " + _segmentMinMax[i].Item2.y + " " + _segmentMinMax[i].Item2.z);
+    }
 
-        Instantiate(GO_point_min, _segmentMinMax[i].Item1, Quaternion.identity);
-        Instantiate(GO_point_max, _segmentMinMax[i].Item2, Quaternion.identity);
+    float SqrDistance(Vector3 p1, Vector3 p2)
+    {
+        float x = p2.x - p1.x;
+        float y = p2.y - p1.y;
+        float z = p2.z - p1.z;
+        return x * x + y * y + z * z;
+    }
+
+    bool adjustJoint(int indexSeg1, int indexSeg2, float epsilon)
+    {
+        List<Vector3> seg1List = new List<Vector3>();
+        seg1List.Add(_segmentMinMax[indexSeg1].Item1);
+        seg1List.Add(_segmentMinMax[indexSeg1].Item2);
+        List<Vector3> seg2List = new List<Vector3>();
+        seg2List.Add(_segmentMinMax[indexSeg2].Item1);
+        seg2List.Add(_segmentMinMax[indexSeg2].Item2);
+        int indexClosestSeg1 = 0;
+        int indexClosestSeg2 = 0;
+
+        float minDistance = float.MaxValue;
+        float distance;
+
+        for (int i = 0; i < 2; ++i)
+        {
+            for (int j = 0; j < 2; ++j)
+            {
+                distance = SqrDistance(seg1List[i], seg2List[j]);
+                if (minDistance > distance)
+                {
+                    indexClosestSeg1 = i;
+                    indexClosestSeg2 = j;
+                    minDistance = distance;
+                }
+            }
+        }
+
+
+
+        if (minDistance < epsilon)
+        {
+            Vector3 center = seg1List[indexClosestSeg1] + (seg2List[indexClosestSeg2] - seg1List[indexClosestSeg1]) / 2;
+            if (indexClosestSeg1 == 0)
+                _segmentMinMax[indexSeg1] = (center, _segmentMinMax[indexSeg1].Item2);
+            else
+                _segmentMinMax[indexSeg1] = (_segmentMinMax[indexSeg1].Item1, center);
+
+            if (indexClosestSeg2 == 0)
+                _segmentMinMax[indexSeg2] = (center, _segmentMinMax[indexSeg2].Item2);
+            else
+                _segmentMinMax[indexSeg2] = (_segmentMinMax[indexSeg2].Item1, center);
+            return true;
+        }
+        else
+            return false;
+    }
+
+    void makeRig()
+    {
+        adjustJoint(0, 1, _epsilon);
+        adjustJoint(2, 3, _epsilon);
+        adjustJoint(4, 5, _epsilon);
+        adjustJoint(6, 7, _epsilon);
+        adjustJoint(6, 7, _epsilon);
+        adjustJoint(7, 8, _epsilon);
+        adjustJoint(9, 10, _epsilon);
+        adjustJoint(10, 11, _epsilon);
     }
 
 
@@ -82,6 +148,7 @@ public class acp_create_bones : MonoBehaviour
         {
             //Child's transform
             Transform trans = gameObject.transform.GetChild(i);
+            print(i + " " + trans);
             _trans.Add(trans);
 
             //Childs'mesh
@@ -102,6 +169,14 @@ public class acp_create_bones : MonoBehaviour
                 mesh_vertices = fillMeshVertices(_trans[i], _meshFilter[i]);
                 ACP_CreateSegment(mesh_vertices, i);
             }
+        }
+
+        makeRig();
+
+        for(int i=0; i<_segmentMinMax.Count; i++)
+        {
+            Instantiate(GO_point_min, _segmentMinMax[i].Item1, Quaternion.identity);
+            Instantiate(GO_point_max, _segmentMinMax[i].Item2, Quaternion.identity);
         }
 
     }
